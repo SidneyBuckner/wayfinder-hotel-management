826d7ca812e7451fb1ca7e1e77390f20cb5246af
Sidney Buckner <sidney.buckner2@gmail.com>
2024-04-03 19:04:51 -0400

    Header nd Footer Component Placeholders to remind myself to make them later. Experimenting with fonts to see how each look like. Chose Fira for obvious reasons.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,33 @@
-import { Metadata } from "next";
-import {  Fira_Sans, Voltaire } from "next/font/google";
-import "./globals.css";
+import { Metadata } from 'next';
+import { Fira_Code } from 'next/font/google';
+import './globals.css';
+import React from 'react';
+import Header from '@/components/Header/Header';
 
-const fira = Fira_Sans({ subsets: ["latin"], weight: ['300','400','500','700', '900'], style:['italic', 'normal'], variable: '--font-fira' });
-const voltaire = Voltaire({subsets: ["latin"], weight: ['400'], style:[ 'normal'], variable: '--font-voltaire' });
+
+const fira = Fira_Code({ subsets: [ 'latin' ], weight: [ '300', '400', '500','700' ], style: [ 'normal' ], variable: '--font-fira' });
+// const lora  = Lora({ subsets: [ 'latin' ], weight: [ '400' ], style: [ 'italic', 'normal' ], variable: '--font-lora' });
+
 export const metadata: Metadata = {
-  title: "Wayfinder - Hotel Management",
-  description: "A Kingdom Hearts themed hotel management system ",
+    'title': 'Wayfinder - Hotel Management',
+    'description': 'A Kingdom Hearts themed hotel management system ',
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+    children,
+}: {
   children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en" className={`${fira.variable} ${voltaire.variable}`}>
-      <body >{children}</body>
-    </html>
-  );
+}){
+    return (
+        <html lang="en" >
+            <body className={fira.className}>
+                <main className="font-normal">
+                    <Header/>
+                    {children}
+                    {/* Footer Component */}
+                </main>
+            </body>
+
+        </html>
+    );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,20 @@
+
+const Header = () => (
+    <header>
+        <div>
+            <p>Wayfinder - A world away from home.</p>
+            <ul>
+                <li>
+    
+                </li>
+                <li>
+                    
+                </li>
+                <li>
+                    
+                </li>
+            </ul>
+        </div>
+    </header>);
+
+export default Header;
