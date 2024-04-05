5913d835a7c09c3387fca7988d49111e0f4fd84e
Sidney Buckner <sidney.buckner2@gmail.com>
2024-04-05 13:59:17 -0400

    Attempting to figure out what I did wrong with my Tailwind styles. This is BEFORE:

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,19 @@
+import Link from 'next/link';
 
 const Header = () => (
-    <header>
-        <div>
-            <p>Wayfinder - A world away from home.</p>
-            <ul>
+    <header className="py-10 px-4 mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between ">
+        <div className="flex items-center w-full md:2/3">
+            {/* <p>Wayfinder - A world away from home.</p> */}
+            <Link href="/" className="font-red">Home</Link>
+            <ul className="flex items-center ml-5">
                 <li>
-    
+                    Worlds
                 </li>
                 <li>
-                    
+                    Listings
                 </li>
                 <li>
-                    
+                    Wanna Get Away
                 </li>
             </ul>
         </div>
