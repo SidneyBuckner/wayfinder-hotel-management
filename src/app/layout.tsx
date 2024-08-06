320944c90c708a5c72d15250b87785d02307eb45
Sidney Buckner <sidney.buckner2@gmail.com>
2024-08-06 17:11:29 -0400

    Starting Over with the Context API becuase there is something Im truly missing from what Im doing here.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,10 @@ import './globals.css';
 import React from 'react';
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
+import ColorThemeProvider from '@/components/ColorThemeProvider/ColorThemeProvider';
 
 
-const fira = Fira_Code({ subsets: [ 'latin' ], weight: [ '300', '400', '500','700' ], style: [ 'normal' ], variable: '--font-fira' });
+const fira = Fira_Code({ 'subsets': [ 'latin' ], 'weight': [ '300', '400', '500','700' ], 'style': [ 'normal' ], 'variable': '--font-fira' });
 // const lora  = Lora({ subsets: [ 'latin' ], weight: [ '400' ], style: [ 'italic', 'normal' ], variable: '--font-lora' });
 
 export const metadata: Metadata = {
@@ -22,12 +23,14 @@ children: React.ReactNode;
     return (
         <html lang="en" >
             <body className={fira.className}>
-                <main className="font-normal">
-                    <Header/>
-                    {children}
-                    <Footer/>
-                    {/* Footer Component */}
-                </main>
+                <ColorThemeProvider>
+                    <main className="font-normal">
+                        <Header/>
+                        {children}
+                        <Footer/>
+                        {/* Footer Component */}
+                    </main>
+                </ColorThemeProvider>
             </body>
         </html>
     );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,34 +1,42 @@
 import Link from 'next/link';
 import { IoMdSettings } from 'react-icons/io';
 import { FaHeart } from 'react-icons/fa';
+import { useContext } from 'react';
+import ColorThemeContext from '@/context/colorThemeContext';
 
-const Header = () => (
-    <header className="py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-wrap items-center justify-between">
-        <div className="flex items-center w-full md:2/3">
-            <p><Link href="/" className="text-tertiary-dark"> Wayfinder - A World Away From Home</Link></p>
-        </div>
-        <div>
-            <ul className="flex items-center">
-                <li className="ml-5 cursor-pointer hover:-translate-y-2 duration-500 transition-all"><Link href="/worlds">Worlds</Link></li>
-                <li className="ml-5 cursor-pointer hover:-translate-y-2 duration-500 transition-all"><Link href="/rooms">Rooms</Link></li>
-                <li className="ml-5 cursor-pointer hover:-translate-y-2 duration-500 transition-all"><Link href="/about">About</Link></li>
-                <li className="ml-5 cursor-pointer hover:-translate-y-2 duration-500 transition-all"><Link href="/contact">Contact</Link></li>
-            </ul>
-        </div>
-        <div>
-            <ul className="flex items-center">
-                <li className="ml-5 cursor-pointer hover:-translate-y-2 duration-500 transition-all">
-                    <Link href="/auth">
-                        <IoMdSettings className="cursor-pointer"/>
-                    </Link>
-                </li>
-                <li className="ml-5 cursor-pointer hover:-translate-y-2 duration-500 transition-all">
-                    <a href="/">  {/* Replace with your desired link */}
-                        <FaHeart className="cursor-pointer" alt="Light Mode" />
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </header>);
+const Header = () => {
+    const { darknessTheme, setDarknessTheme } = useContext(ColorThemeContext);
+
+    return (
+        <header className="py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-wrap items-center justify-between">
+            <div className="flex items-center w-full md:2/3">
+                <p><Link href="/" className="text-tertiary-dark"> Wayfinder - A World Away From Home</Link></p>
+            </div>
+            <div>
+                <ul className="flex items-center">
+                    <li className="ml-5 cursor-pointer hover:-translate-y-2 duration-500 transition-all"><Link href="/worlds">Worlds</Link></li>
+                    <li className="ml-5 cursor-pointer hover:-translate-y-2 duration-500 transition-all"><Link href="/rooms">Rooms</Link></li>
+                    <li className="ml-5 cursor-pointer hover:-translate-y-2 duration-500 transition-all"><Link href="/about">About</Link></li>
+                    <li className="ml-5 cursor-pointer hover:-translate-y-2 duration-500 transition-all"><Link href="/contact">Contact</Link></li>
+                </ul>
+            </div>
+            <div>
+                <ul className="flex items-center">
+                    <li className="ml-5 cursor-pointer hover:-translate-y-2 duration-500 transition-all">
+                        <Link href="/auth">
+                            <IoMdSettings className="cursor-pointer"/>
+                        </Link>
+                    </li>
+                    <li className="ml-5 cursor-pointer hover:-translate-y-2 duration-500 transition-all">
+                        {/* <a href="/">   */}
+                        {/* Replace with your desired link */}
+                            {/* <FaHeart className="cursor-pointer" alt="Light Mode" /> */}
+                        {darknessTheme ? <FaHeart className="cursor-pointer" alt="Light Mode" />: <FaHeart className="cursor-pointer" alt="Light Mode" />}
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </header>);
+};
 
 export default Header;
